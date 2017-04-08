e9e09374cbbda97a428575ac0dc96c22d54c237c
lafinglion <lafinglion@yahoo.com>
2017-04-07 17:04:51 -0700

    Update plot2.R

--- plot2.R
+++ plot2.R
@@ -1,10 +1,13 @@
+# Read data set
 file <- "./data/household_power_consumption.txt"
 data <- read.table(file, header = TRUE, sep = ";", stringsAsFactors = FALSE)
 subsetData <- data[(data$Date == "1/2/2007") | (data$Date == "2/2/2007"),]
 
+# Convert Date and Time variables
 datetime <- paste(subsetData$Date, subsetData$Time, sep = " ")
 DateTime <- strptime(datetime, "%d/%m/%Y %H:%M:%S")
 
+# Make plot and create file
 GAP <- as.numeric(subsetData$Global_active_power)
 plot(DateTime, GAP, type = "l", xlab = "", ylab = "Global Active Power (kilowatts)")
 dev.copy(png, file="./data/plot2.png", height=480, width=480)
