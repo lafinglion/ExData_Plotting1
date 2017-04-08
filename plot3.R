542b16c084a150acf2f40a20884f0347df0bf3f2
lafinglion <lafinglion@yahoo.com>
2017-04-07 17:08:42 -0700

    Update plot3.R

--- plot3.R
+++ plot3.R
@@ -1,14 +1,18 @@
+# Read data set
 file <- "./data/household_power_consumption.txt"
 data <- read.table(file, header = TRUE, sep = ";", stringsAsFactors = FALSE)
 subsetData <- data[(data$Date == "1/2/2007") | (data$Date == "2/2/2007"),]
 
+# Convert Date and Time varialbes
 datetime <- paste(subsetData$Date, subsetData$Time, sep = " ")
 DateTime <- strptime(datetime, "%d/%m/%Y %H:%M:%S")
 
+# Convert to numeric value
 subMeter1 <- as.numeric(subsetData$Sub_metering_1)
 subMeter2 <- as.numeric(subsetData$Sub_metering_2)
 subMeter3 <- as.numeric(subsetData$Sub_metering_3)
 
+# Make plot and create file
 plot(DateTime, subMeter1, type = "l", xlab = "", ylab="Energy sub metering")
 lines(DateTime, subMeter2, type = "l", col="red")
 lines(DateTime, subMeter3, type = "l", col="blue")
